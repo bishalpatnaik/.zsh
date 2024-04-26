dcab4f5f8758161a1b0d733f9603cb85f2a19f1f
Bishal Patnaik <bishalpatnaik@bishals-mac.local>
2024-04-27 01:41:04 +0530

    first commit

---
+++ config/aliases.zsh
@@ -0,0 +1,11 @@
+# Aliases
+
+# ---- switched to abbreviations ------------
+# alias gst="git status"
+# alias yst="yadm status"
+# alias grep='grep --color'
+
+# alias ls="exa --icons --group-directories-first -l"
+# alias ll="exa --icons --group-directories-first -la"
+alias ..="cd .."
+alias ...="cd ../.."
